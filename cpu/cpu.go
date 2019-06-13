dd2176b9081cc6a3f898b1b28b01a42169c5841e
Aleksandr Vaizman <vaizmanai@mail.ru>
2019-06-14 00:56:10 +0300

    Update cpu.go

--- cpu/cpu.go
+++ cpu/cpu.go
@@ -9,7 +9,7 @@ import (
 	"sync"
 	"time"
 
-	"github.com/shirou/gopsutil/internal/common"
+	"github.com/vaizmanai/gopsutil/internal/common"
 )
 
 // TimesStat contains the amounts of time the CPU has spent performing different
